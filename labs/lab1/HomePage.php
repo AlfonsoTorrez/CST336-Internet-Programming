74a7771bef3e74148e6e89f6ba599726dc5f60f8
Alfonso T <alftorres@csumb.edu>
2017-09-01 00:15:10 +0000

    Hey

---
+++ labs/lab1/HomePage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title> Alfonso Torres: Personal Website </title>
+    <meta charset="utf-8"/>
+</head>
+
+<body style="background-color:black; color:white">
+    <header>
+        <h1>Alfonso Torres</h1>
+    </header>
+    
+    <nav>
+        <a href="/alftorres/labs/lab1/HomePage.php">Home</a>
+        <a href="/alftorres/labs/lab1/AboutMe.php">About</a>
+        <a href="/alftorres/labs/lab1/Contact.php">Contact</a>
+        <hr>
+    </nav>
+    
+    <main>
+    
+    <figure>
+        <img src="/alftorres/labs/lab1/150406648527202.jpg" alt="Image of Alfonso">
+    </figure>    
+    <div>
+        <p>Welcome!</p>
+        <p>Thank you for visiting my personal portfolio website.</p>
+        <p>I am a Software Engineer and I have been recently hired at Pied Pipper.</p>
+        <p>Feel free to contact me!</p>
+        
+        <em>"Our prime purpose in this life is to help others. 
+        And if you can't help them, at least don't hurt them."</em>
+        <p>- Dalai Lama</p>
+    </div> 
+    
+    <footer>
+        <hr>
+        CST 336, 2017 &copy; Torres <br /> 
+        <strong>Disclaimer:</strong> The information on this webpage is fictitous. <br /> 
+        Its is used for academic purpose. 
+    </footer>
+    
+    </main>
+</body>
+
+</html>
