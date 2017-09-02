a9247d2147af68508a3ad55c07506969be0c64f6
Alfonso T <alftorres@csumb.edu>
2017-09-02 07:36:48 +0000

    Final Changes

--- index.php
+++ index.php
@@ -1,52 +1,9 @@
 <!DOCTYPE html>
-
 <html>
-
-<head>
-    <meta charset="utf-8"/>
-    <title> Alfonso Torres: Personal Website </title>
-    <link href="css/styles.css" rel="stylesheet" type="text/css" /> 
-</head>
-
-
-
-<body>
-    <header>
-        <h1>Alfonso Torres</h1>
-    </header>
-
-    <nav>
-        <hr width="25%"/>
-        <a class="home" href="/alftorres/labs/lab1/homePage.html" >Home</a>
-        <a href="/alftorres/labs/lab1/about.html">About</a>
-        <a href="/alftorres/labs/lab1/contact.html">Contact</a>
-    </nav>
-    
-    <main>
-    
-    <figure id="me">
-        <img src="/alftorres/labs/lab1/150406648527202.jpg" alt="Image of Alfonso">
-    </figure>    
-    <div id="welcomeText">
-        <p>Welcome!</p>
-        <p>Thank you for visiting my personal portfolio website.</p>
-        <p>I am a Software Engineer and I have been recently hired at Pied Pipper.</p>
-        <p>Feel free to contact me!</p>
-        
-        <em>"Our prime purpose in this life is to help others. 
-        And if you can't help them, at least don't hurt them."</em>
-        <p>- Dalai Lamsa</p>
-    </div> 
-    
-    <footer>
-        <hr>
-        CST 336, 2017 &copy; Torres <br /> 
-        <strong>Disclaimer:</strong> The information on this webpage is fictitous.<br /> 
-        Its is used for academic purpose.<br />
-        <img src="/alftorres/labs/lab1/CSUMBPIC.png" alt="CSUMB Logo">
-    </footer>
-    
-    </main>
-</body>
-
+    <head>
+        <title></title>
+    </head>
+    <body>
+    <p>Welcome!</p>
+    </body>
 </html>
