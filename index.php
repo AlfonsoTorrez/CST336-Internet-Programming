e8b5080b930fc522f74f0d2d8c4ce244bf828805
Alfonso T <alftorres@csumb.edu>
2017-09-11 04:02:31 +0000

    Lab 2 Again

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <title>Welcome Page!</title>
     </head>
     <body>
-        <a href="http://alftorres-cst336.herokuapp.com/labs/lab1/homePage.html" >Welcome!</a>
+        <a href="http://alftorres-cst336.herokuapp.com/labs/lab1/homePage.html" >Lab1</a>
+        <br>
+        <a href="http://alftorres-cst336.herokuapp.com/labs/lab2/index.html" >Lab2!</a>
     </body>
 </html>
