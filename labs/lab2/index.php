367df0888071b1a71ed428c2665805d6da456daa
Alfonso T <alftorres@csumb.edu>
2017-09-11 06:45:20 +0000

    Hey

--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -1,5 +1,5 @@
 <?php 
-    include 'http://alftorres-cst336.herokuapp.com/labs/lab2/inc/functions.php'; 
+    include '/home/ubuntu/workspace/alftorres/labs/lab2/inc/functions.php'; 
 ?>
 <!DOCTYPE html>
 <html>
