c4aee3e16d63004207065b6a03f5460f08fff50d
Alfonso T <alftorres@csumb.edu>
2017-09-11 03:51:00 +0000

    Lab 2 Push

--- inc/functions.php
+++ inc/functions.php
@@ -1,32 +1,95 @@
 <?php
 
-    function displaySymbol($randomValue){
+    function displaySymbol($randomValue1,$randomValue2,$randomValue3){
             
-            switch($randomValue){
-                case 0: $symbol7 = "seven"; 
+            switch($randomValue1){
+                case 0: $symbol1 = "seven"; 
                         break; 
-                case 1: $symbol7 = "grapes"; 
+                case 1: $symbol1 = "bar"; 
                         break; 
-                case 2: $symbol7 = "orange"; 
+                case 2: $symbol1 = "cherry"; 
                         break;
-                case 3: $symbol7 = "bar"; 
+                case 3: $symbol1 = "grapes"; 
                         break;
-                case 4: $symbol7 = "cherry"; 
+                case 4: $symbol1 = "orange"; 
                         break; 
-                case 5: $symbol7 = "lemon"; 
+                case 5: $symbol1 = "lemon"; 
                         break; 
                         
             }
-                    echo "<img src='img/$symbol7.png' alt='$symbol7 Image' title='$symbol7' width='70'/>";  
+            switch($randomValue2){
+                case 0: $symbol2 = "seven"; 
+                        break; 
+                case 1: $symbol2 = "bar"; 
+                        break; 
+                case 2: $symbol2 = "cherry"; 
+                        break;
+                case 3: $symbol2 = "grapes"; 
+                        break;
+                case 4: $symbol2 = "orange"; 
+                        break; 
+                case 5: $symbol2 = "lemon"; 
+                        break; 
+                        
+            }
+            switch($randomValue3){
+                case 0: $symbol3 = "seven"; 
+                        break; 
+                case 1: $symbol3 = "bar"; 
+                        break; 
+                case 2: $symbol3 = "cherry"; 
+                        break;
+                case 3: $symbol3 = "grapes"; 
+                        break;
+                case 4: $symbol3 = "orange"; 
+                        break; 
+                case 5: $symbol3 = "lemon"; 
+                        break; 
+                        
+            }
+                echo "<img id='reel1' src='img/$symbol1.png' alt='$symbol1 Image' title='$symbol1' width='70'/>";  
+                echo "<img id='reel2' src='img/$symbol2.png' alt='$symbol2 Image' title='$symbol2' width='70'/>"; 
+                echo "<img id='reel3' src='img/$symbol3.png' alt='$symbol3 Image' title='$symbol3' width='70'/>"; 
         }
         
+        function displayPoints($randomValue1,$randomValue2,$randomValue3){
+            
+            echo "<div id='output'>"; 
+            
+            if($randomValue1==$randomValue2&&$randomValue1==$randomValue3){
+                switch($randomValue1){
+                    case 0: $totalPoints = 1000; 
+                            echo "<h1>Jackpot!</h1>";
+                            break; 
+                    case 1: $totalPoints = 500; 
+                            break; 
+                    case 2: $totalPoints = 250; 
+                            break;
+                    case 3: $totalPoints = 150; 
+                            break;
+                    case 4: $totalPoints = 75; 
+                            break; 
+                    case 5: $totalPoints = 25; 
+                            break; 
+                }
+                echo "<h2>You won $totalPoints points!</h2>"; 
+            }
+            else{
+                  echo "<h3>Try again :(</h3>";
+            }
+            echo "</div>"; 
+        }
         
-        $randomValue1 = rand(0,5); 
-        displaySymbol($randomValue1);
-        $randomValue2 = rand(0,5); 
-        displaySymbol($randomValue2);
-        $randomValue3 = rand(0,5); 
-        displaySymbol($randomValue3);
+        function play(){
+            $randomValue1 = rand(0,5); 
+            $randomValue2 = rand(0,5); 
+            $randomValue3 = rand(0,5);
+            
+            displaySymbol($randomValue1,$randomValue2,$randomValue3);
+            
+            displayPoints($randomValue1,$randomValue2,$randomValue3); 
+            
+            //echo $randomValue1 . " " . $randomValue2 . " " . $randomValue3;
+        }
         
-        echo $randomValue1 . " " . $randomValue2 . " " . $randomValue3; 
 ?>
--- labs/lab2/index.php
+++ labs/lab2/index.php
@@ -1,14 +1,36 @@
-<?php
-    include '/inc/functions.php';
+<?php 
+    include '/home/ubuntu/workspace/alftorres/inc/functions.php'; 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title> 777 Slot Machine </title>
-        <meta charset="utf-8"/> 
+        <meta charset="utf-8"/>
+        <style> 
+            @import url("/alftorres/labs/lab2/css/styles.css"); 
+        </style>
     </head>
-
+    
     <body>
+        <header>
+        <p class="rules"> <u>Rules</u>:<br>
+            <u>Set of 7's</u>: 1000 points <br>
+            <u>Set of Bar's</u>: 500 points <br>
+            <u>Set of Cherries</u>: 250 points <br>
+            <u>Set of Grapes</u>: 150 points <br>
+            <u>Set of Oranges</u>: 75 points <br>
+            <u>Set of Lemons</u>: 25 points <br>
+        </p>
+        </header>
+        <div id="main"> 
+            <?php
+                play(); 
+            ?> 
+            
+            <form id="spinButton"> 
+                <button name="subject" type="submit" value="Spin!">Spin!</button>
+            </form>
+        </div>
         
     </body>
 </html>
