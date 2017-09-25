6525247643113e3e4ef6aaf4757554e4d5ff3458
Alfonso T <alftorres@csumb.edu>
2017-09-25 02:24:47 +0000

    fixing index

--- index.php
+++ index.php
@@ -4,14 +4,31 @@
     <head>
         <meta charset="utf-8"/>
         <title>Welcome Page!</title>
+        <link href="indexCSS.css" rel="stylesheet" type="text/css" /> 
     </head>
     <body>
-        <a href="http://alftorres-cst336.herokuapp.com/labs/lab1/homePage.html" >Lab1</a>
-        <br>
-        <a href="http://alftorres-cst336.herokuapp.com/homework/hw1/teachingCS.html" >Homework1</a>
-        <br>
-        <a href="http://alftorres-cst336.herokuapp.com/labs/lab2/index.php" >Lab2</a>
-        <br>
-        <a href="http://alftorres-cst336.herokuapp.com/labs/lab3/index.php" >Lab3</a>
+        <nav> 
+            <a href="http://alftorres-cst336.herokuapp.com/homework/hw1/teachingCS.html">Homework 1</a>
+            <br>
+            <a href="http://alftorres-cst336.herokuapp.com/homework/hw2/randomFacts.php">Homework 2</a>
+            <br>
+            <a href="http://alftorres-cst336.herokuapp.com/labs/lab1/homePage.html" >Lab 1</a>
+            <br>
+            <a href="http://alftorres-cst336.herokuapp.com/labs/lab2/index.php" >Lab 2</a>
+            <br> 
+            <a href="http://alftorres-cst336.herokuapp.com/labs/lab3/index.php" >Lab 3</a>
+            <br>
+            <a href="http://alftorres-cst336.herokuapp.com/labs/lab4/main.php" >Lab 4</a>
+            <br>
+        </nav>
+        
+        <footer>
+            <hr>
+            CST 336, 2017 &copy; Torres <br /> 
+            <strong>Disclaimer:</strong> The information on this webpage is fictitous.<br /> 
+            Its is used for academic purpose.<br />
+            <img src="http://alftorres-cst336.herokuapp.com/labs/lab1/CSUMBPIC.png" alt="CSUMB Logo">
+        </footer>
+    
     </body>
 </html>
