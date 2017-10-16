6a157ab9a5229b8505f3b35b6d7bb37556cd49de
Alfonso T <alftorres@csumb.edu>
2017-10-16 06:40:25 +0000

    Lab 5

---
+++ labs/lab5/functions.php
@@ -0,0 +1,130 @@
+<?php
+
+$conn = getDatabaseConnection();
+//Device Info
+$deviceNameInOrder = array(); 
+$deviceId = array();
+$deviceName = array(); 
+$deviceType = array();
+$devicePrice = array();
+$deviceStatus = array(); 
+
+//Checkout Info 
+$cId = array();
+$cDate = array();
+$cDueDate = array();
+$cReturnDate = array();
+
+
+function setDeviceInfo() {
+    global $conn;
+    global $deviceId;
+    global $deviceName;
+    global $deviceType;
+    global $devicePrice;
+    global $deviceStatus; 
+    
+    global $cId;
+    global $cDate;
+    global $cDueDate;
+    global $cReturnDate;
+    
+    $sql = "SELECT DISTINCT(deviceName), deviceId, 
+            deviceType, price, status FROM `tc_device` 
+            ORDER BY deviceName";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record) {
+        $deviceName[] = $record['deviceName'];
+        $deviceType[] = $record['deviceType'];
+        $deviceId[] = $record['deviceId'];
+        $devicePrice[] = $record['price'];
+        $deviceStatus[] = $record['status'];
+    }
+    
+    
+    $sql = "SELECT deviceId, checkoutDate, dueDate, returnDate FROM `tc_checkout`";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record) {
+        $cId[] = $record['deviceId'];
+        $cDate[] = $record['checkoutDate'];
+        $cDueDate[] = $record['dueDate'];
+        $cReturnDate[] = $record['returnDate'];
+    }
+}
+
+function getDeviceNamesInOrder(){
+    global $deviceName; 
+    
+    for($i=0;$i<sizeOf($deviceName); $i++){
+        echo "$deviceName[$i]"; 
+        echo "<br>"; 
+    }
+}
+
+function getDeviceById($Id){
+    
+    for($i=0; $i<sizeof($deviceType); $i++){
+        if($deviceId[$i]==$Id){
+            echo "ID:$deviceId[$i] ----- NAME:$deviceName[$i] ----- TYPE:$deviceType[$i] ----- PRICE:$devicePrice[$i] ----- STATUS:$deviceStatus[$i]";
+            echo "<br>";
+        }
+    }
+}
+
+function filterByTypeA($a){
+    if($a=="y"){
+        for($i=0; $i<sizeof($deviceType); $i++){
+            if($deviceId[$i]==$Id){
+                echo "ID:$deviceId[$i] ----- NAME:$deviceName[$i] ----- TYPE:$deviceType[$i] ----- PRICE:$devicePrice[$i] ----- STATUS:$deviceStatus[$i]";
+                echo "<br>";
+            }
+        }
+    }
+    else if($a=="n"){
+        
+    }
+}
+
+function filterByType($type){
+    global $deviceId;
+    global $deviceName;
+    global $deviceType;
+    global $devicePrice;
+    global $deviceStatus; 
+    
+    for($i=0; $i<sizeof($deviceType); $i++){
+        if($deviceType[$i]==$type){
+            echo "ID:$deviceId[$i] ----- NAME:$deviceName[$i] ----- TYPE:$deviceType[$i] ----- PRICE:$devicePrice[$i] ----- STATUS:$deviceStatus[$i]";
+            echo "<br>";
+        }
+        
+    }
+}
+
+function filterByName($name){
+    global $deviceId;
+    global $deviceName;
+    global $deviceType;
+    global $devicePrice;
+    global $deviceStatus; 
+    echo '<h3>Devices Filterd This Name: ',$Name,'</h3>';
+    for($i=0;$i<sizeOf($deviceName);$i++){
+        if($deviceName[$i]==$name){
+            echo "ID:$deviceId[$i] ----- NAME:$deviceName[$i] ----- TYPE:$deviceType[$i] ----- PRICE:$devicePrice[$i] ----- STATUS:$deviceStatus[$i]";
+            echo "<br>";
+        }
+    }
+    if (!in_array("$name", $deviceName)) {
+        echo "Name not found in database.";
+    }
+}
+
+?> 
--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -3,30 +3,14 @@
 1) All devices are initially displayed, ordered by name (15pts)
 2) Devices are filtered by type  (15pts)
 3) Devices are filtered by name  (15pts)
-4) Devices are filtered by availability (15pts)
+4) Devices are filtered by availability (15pts) //I need to go to "checkout" part of the database
 5) Devices are sortable by name or price (ascending order) (15pts)
 6) Devices listed include a link to see checked-out history (15pts)
-7) There is an external CSS file (10pts)
+7) There is an external CSS file (10pts) *DONE*
 */
 include '../../dbConnection.php';
-    
-$conn = getDatabaseConnection(); 
-    
-function getDeviceTypes() {
-    global $conn;
-    $sql = "SELECT DISTINCT(deviceType)
-            FROM `tc_device` 
-            ORDER BY deviceType";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    foreach ($records as $record) {
-        echo "<option> "  . $record['deviceType'] . "</option>";
-    }
-}
-    
+include 'functions.php'; 
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,11 +19,75 @@ function getDeviceTypes() {
         <meta charset="utf-8"/>
         <link href="css/styles.css" rel="stylesheet" type="text/css">
     </head>
+    
     <body>
         <h1>Device Center</h1>
+        <form id=deviceNamesInOrder>
+            
+        <?=setDeviceInfo()?>
+        <h2><u>Device Names</u>:</h2>
+        <?=getDeviceNamesInOrder()?> 
+        <br></br>
+        </form>
+        <form id=deviceInfo>
+            Filter Devices By Type: 
+            <select name="deviceTypes">
+                <option value="" >Select One</option>
+                <option value="Camera" >Camera</option>
+                <option value="DynamicMics">Dynamic Mics</option>
+                <option value="Laptop">Laptop</option>
+                <option value="Microphone">Microphone</option>
+                <option value="Smartphone">Smartphone</option>
+                <option value="Tablet">Tablet</option>
+                <option value="VR Headset">VR Headset</option>
+            </select>
+            <br></br>
+            <input type="submit" name="submit" value="Submit">
+            <br></br>
+            
+            <?php
+                if(isset($_GET['submit']) && $_GET['deviceTypes']!==""){
+                    $type = $_GET['deviceTypes']; 
+                    echo '<h3>Devices Filterd By: ',$type,'</h3>';
+                    filterByType($type); 
+                    echo '<br></br>';
+                }
+            ?> 
         
-        <form>
-            <?=getDeviceTypes()?>
+        
+        Filter Devices By Name: 
+            <input type="text" name="textBox" value="">
+            <br></br>
+            <input type="submit" name="submit" value="Submit">
+            <br></br>
+        
+            <?php
+                if(isset($_GET['submit'])&&$_GET['textBox']!==""){
+                    $name = $_GET['textBox']; 
+                    filterByName($name); 
+                }
+            ?> 
+            
+        Filter Devices By Availability: 
+            <select name="deviceAvailability">
+                <option value="" >Select One</option>
+                <option value="" >Available</option>
+                <option value="n" >Not Available</option>
+            </select>
+            <br></br>
+            <input type="submit" name="submit" value="Submit">
+            <br></br>
+            
+            <?php
+                if(isset($_GET['submit']) && $_GET['deviceAvailability']!==""){
+                    $a = $_GET['deviceAvailability']; 
+                    echo '<h3>Devices Filterd By: Availability</h3>';
+                    filterByTypeA($a); 
+                    echo '<br></br>';
+                }
+            ?> 
+        
+
         </form>
     </body>
 </html>
