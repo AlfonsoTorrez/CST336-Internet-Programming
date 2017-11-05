0f4479825c1578a3c19d0e91a5c73b67d06984f3
Alfonso T <alftorres@csumb.edu>
2017-11-05 08:51:11 +0000

    My Website

---
+++ myWebsite/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Alfonso Torres: Personal Website</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Alfonso Torres: Personal Website</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">About Me</a></li>
+              <li><a href="#">Contact Me</a></li>
+            </ul>
+          </div>
+        </nav>
+        <img src="img/applePicture.jpg" class="img-circle" alt="Alfonso Torres">
+        
+    </body>
+</html>
